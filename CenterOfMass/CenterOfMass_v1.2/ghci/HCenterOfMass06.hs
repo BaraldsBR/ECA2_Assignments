56b49a6491faf8df841d7877c744f088bc8ff2ef
BaraldsBR <baraldsbr@gmail.com>
2026-01-24 19:53:11 +0100

    CenterOfMass: exercises 1 and 2

--- CenterOfMass/CenterOfMass_v1.2/ghci/HCenterOfMass06.hs
+++ CenterOfMass/CenterOfMass_v1.2/ghci/HCenterOfMass06.hs
@@ -15,7 +15,12 @@ import Data.List
 -- Assignment 1, From grayscale to black and white
 -----------------------------------------------------------------------------------------
 
-thresholdIm = undefined
+threshold = 128 -- not sure if we should import this from Common.hs, since that depends focuses on hardware and this is purely functional
+
+thresholdIm :: [[Pixel]] -> [[Pixel]]
+thresholdIm grayscaleImage = bwImage where
+    applyThreshold p = if p < threshold then 0 else 1
+    bwImage = map (map applyThreshold) grayscaleImage
 
 lightHouseBW :: FilePath
 lightHouseBW = "../images/lightHouseBW.pgm"
@@ -23,6 +28,26 @@ lightHouseBW = "../images/lightHouseBW.pgm"
 -----------------------------------------------------------------------------------------
 -- Assignment 2, Center of mass of rows and picture
 -----------------------------------------------------------------------------------------
+
+comRows :: [[Pixel]] -> Int
+comRows image = rowIndex where
+    rowIndex = div totalProductSum totalSum
+    
+    rowSums = map sum image
+    totalSum = sum rowSums
+
+    productRowSums = zipWith (*) rowSums [0..]
+    totalProductSum = sum productRowSums
+
+com :: [[Pixel]] -> (Int, Int)
+com image = (rowIndex, colIndex) where
+    rowIndex = comRows image
+    colIndex = comRows $ transpose image
+
+imageWithCom :: Int -> [[Pixel]] -> [[Pixel]]
+imageWithCom color image = modifiedImage where
+    (rowIndex, colIndex) = com image
+    modifiedImage = changePixelInImage image rowIndex colIndex color
 
 -----------------------------------------------------------------------------------------
 -- Assignment 3 Center of mass of parts of the image, with and without borders
